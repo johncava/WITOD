53f938b9789c9c3779b94970ff584c0d1a6b78c6
johncava <jcava@asu.edu>
2019-01-07 16:48:32 -0700

    benchmark changes

---
+++ benchmark_dataset/benchmark.py
@@ -0,0 +1,68 @@
+import argparse
+import os
+import glob
+
+###
+# All python scripts called in this script are done in accordance for a specific column (sample) from the original OTU Table.
+# This is in order to segregate and make sure that sequences associated in one sample aren't considered in another sample if it isn't there.
+###
+
+parser = argparse.ArgumentParser()                                              
+                                                                                
+parser.add_argument("-input",help= "Input OTU Table file name")                 
+parser.add_argument("-out", help= "Output OTU Table file name")                     
+parser.add_argument("-rep_set", help= "Rep Set Path")         
+                                                                                
+args = parser.parse_args()
+
+sample_otu_table_name = "SAMPLE_OTU_TABLE"
+otu_file = args.input
+numColumns = None
+samples = None
+with open(otu_file, 'r') as f:
+    samples = f.readline().split('\t')
+    numColumns = len(samples)
+
+##########
+# For Loop for each Sample
+for column in range(1,numColumns - 1):
+
+    WORKING_DIRECTORY="./" + samples[column]
+                                                                                    
+    os.system("mkdir " + WORKING_DIRECTORY)                                                      
+    os.system("mv filter_v3.py "+ WORKING_DIRECTORY)                                            
+    os.system("mv otu_filter_1.py " + WORKING_DIRECTORY)                                         
+    os.system("mv script_v6.py "+ WORKING_DIRECTORY)                                            
+    os.system("mv post.py "+ WORKING_DIRECTORY)                                                 
+    os.system("mv otu_v1.py "+ WORKING_DIRECTORY)                                               
+    os.system("mv otu_filter_2.py "+ WORKING_DIRECTORY)                                         
+    os.system("mv blast.py " + WORKING_DIRECTORY)                                                
+    os.system("mv otu_silva_v4.py "+ WORKING_DIRECTORY)                                         
+    os.system("mv final.py "+ WORKING_DIRECTORY)
+    os.system("mv diversity_v2.py "+ WORKING_DIRECTORY)
+                                                                                    
+    os.chdir('./' + WORKING_DIRECTORY)                                                         
+                                                                                    
+    os.system("python filter_v3.py " + args.rep_set + " " + args.input + " " + str(column))
+    print('Stage 1 Complete')
+    os.system("python -W ignore otu_filter_1.py")
+    print('Stage 2 Complete')                                                
+    os.system("python script_v6.py")
+    print('Stage 3 Complete')                                                             
+    os.system("python post.py")
+    print('Stage 4 Complete')                                                                  
+    os.system("python otu_v1.py")
+    print('Stage 5 Complete')                                                                
+    os.system("python -W ignore otu_filter_2.py")
+    print('Stage 6 Complete')                                                                                                                 
+                                                                                    
+    os.system("mv filter_v3.py ./../")                                                           
+    os.system("mv otu_filter_1.py ./../")                                                        
+    os.system("mv script_v6.py ./../")                                                           
+    os.system("mv post.py ./../")                                                                
+    os.system("mv otu_v1.py ./../")                                                              
+    os.system("mv otu_filter_2.py ./../")                                                        
+
+    os.chdir('./../')
+
+print('Done')
---
+++ benchmark_dataset/benchmark.sh
@@ -0,0 +1,14 @@
+#!/bin/bash                                                                     
+                                                                                
+#SBATCH -n 16                        # number of cores                           
+#SBATCH -t 2-12:00                  # wall time (D-HH:MM)                       
+#SBATCH -o slurm.%j.out             # STDOUT (%j = JobId)                       
+#SBATCH -e slurm.%j.err             # STDERR (%j = JobId)                       
+#SBATCH --mail-type=ALL             # Send a notification when the job starts, stops, or fails
+#SBATCH --mail-user=jcava@asu.edu # send-to address
+
+module load python/3.6.4                                                        
+module load mafft/7.402
+
+python witod.py -input OTU_TABLE.txt -out SAMPLE_OTU_TABLE -rep_set rep_set.fna
+
